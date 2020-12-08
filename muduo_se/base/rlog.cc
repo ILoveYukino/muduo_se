ca9742e6b9ccb6c05e5b3f856814795ac69ec3de
805256689 <805256689@qq.com>
2020-12-08 22:20:28 +0800

    master

--- muduo_se/base/rlog.cc
+++ muduo_se/base/rlog.cc
@@ -8,12 +8,6 @@
 #include <stdarg.h>
 #include <cstring>
 
-#define MEM_USE_LIMIT (3u * 1024 * 1024 * 1024)//3GB
-#define LOG_USE_LIMIT (1u * 1024 * 1024 * 1024)//1GB
-#define LOG_LEN_LIMIT (4 * 1024)//4K
-#define RELOG_THRESOLD 5
-#define BUFF_WAIT_TIME 1
-
 std::mutex rlog::mutex_;
 std::condition_variable rlog::cond_;
 uint64_t rlog::one_buff_len = 30*1024*1024;//30MB
@@ -91,72 +85,6 @@ void rlog::persist(){
         //std::cout<<8<<std::endl;
         lock.lock();
         prst_buf=prst_buf->next;
-    }
-}
-
-template<class... Args>
-void rlog::try_append(const char* lv,const char* format,Args&&... args){
-    int ms;
-    uint64_t curr_sec=tm_.get_now_time(&ms);
-    if(lst_lts && curr_sec-lst_lts<RELOG_THRESOLD)
-        return;
-    
-    char log_line[LOG_LEN_LIMIT];
-    int prev_len=snprintf(log_line,LOG_LEN_LIMIT,"%s[%s.%03d]",lv,tm_.fmt_,ms);
-    
-    int main_len = snprintf(log_line+prev_len,LOG_LEN_LIMIT - prev_len,format,convert(std::forward<Args>(args))...);
-    
-    uint32_t len_all = prev_len+main_len;
-
-    lst_lts = 0;
-    bool tell_back=false;
-
-    {
-        std::unique_lock<std::mutex> lock(mutex_);
-        if(cur_buf->status==cell_buffer::FREE && cur_buf->avail_len()>=len_all){
-            cur_buf->append(log_line,len_all);
-        }
-        else{          
-            if(cur_buf->status==cell_buffer::FREE){
-                /*当前缓冲buffer free 但 空余区域小于len*/
-                cur_buf->status=cell_buffer::FULL;
-                cell_buffer* next_buf=cur_buf->next;
-                tell_back=true;
-
-                if(next_buf->status==cell_buffer::FULL){
-                    /*第next个buffer有日志线程在工作*/
-                    if(one_buff_len*(_buf_cnt+1)>MEM_USE_LIMIT){
-                        /*buffer不可以无限制的加，当到一定阈值的时候，异常退出*/
-                        cur_buf=cur_buf->next;
-                        lst_lts=curr_sec;
-                    }
-                    else
-                    {
-                        /*第next个buffer日志线程在工作，因此添加一个buffer*/
-                        cell_buffer* new_buf = new cell_buffer(one_buff_len);
-                        new_buf->next=next_buf;
-                        cur_buf->next=new_buf;
-                        next_buf->pre=new_buf;
-                        new_buf->pre=cur_buf;
-                        cur_buf=cur_buf->next;
-                        _buf_cnt++;
-                    }
-                }
-                else{
-                    /*第next个buffer没有日志线程，数据写到此缓冲区上*/
-                    cur_buf=cur_buf->next;
-                } 
-                if(!lst_lts)
-                        cur_buf->append(log_line,len_all);
-            }
-            else{
-                /*当前buffer消费者正在把数据写到日志文件中，因为是循环链表，故此时所有的buffer均已用完，极少出现*/
-                lst_lts=curr_sec;
-            }
-        }
-    }
-    if(tell_back){
-        cond_.notify_one();
     }
 }
 
--- muduo_se/base/rlog.h
+++ muduo_se/base/rlog.h
@@ -19,6 +19,12 @@ enum LOG_LEVEL{
     TARCE,
 };
 
+#define MEM_USE_LIMIT (3u * 1024 * 1024 * 1024)//3GB
+#define LOG_USE_LIMIT (1u * 1024 * 1024 * 1024)//1GB
+#define LOG_LEN_LIMIT (4 * 1024)//4K
+#define RELOG_THRESOLD 5
+#define BUFF_WAIT_TIME 1
+
 class rlog{
     public:
         /*单例模式*/
@@ -79,6 +85,72 @@ class rlog{
         std::thread tid;
         std::atomic_bool running;
 };
+
+template<class... Args>
+void rlog::try_append(const char* lv,const char* format,Args&&... args){
+    int ms;
+    uint64_t curr_sec=tm_.get_now_time(&ms);
+    if(lst_lts && curr_sec-lst_lts<RELOG_THRESOLD)
+        return;
+    
+    char log_line[LOG_LEN_LIMIT];
+    int prev_len=snprintf(log_line,LOG_LEN_LIMIT,"%s[%s.%03d]",lv,tm_.fmt_,ms);
+    
+    int main_len = snprintf(log_line+prev_len,LOG_LEN_LIMIT - prev_len,format,convert(std::forward<Args>(args))...);
+    
+    uint32_t len_all = prev_len+main_len;
+
+    lst_lts = 0;
+    bool tell_back=false;
+
+    {
+        std::unique_lock<std::mutex> lock(mutex_);
+        if(cur_buf->status==cell_buffer::FREE && cur_buf->avail_len()>=len_all){
+            cur_buf->append(log_line,len_all);
+        }
+        else{          
+            if(cur_buf->status==cell_buffer::FREE){
+                /*当前缓冲buffer free 但 空余区域小于len*/
+                cur_buf->status=cell_buffer::FULL;
+                cell_buffer* next_buf=cur_buf->next;
+                tell_back=true;
+
+                if(next_buf->status==cell_buffer::FULL){
+                    /*第next个buffer有日志线程在工作*/
+                    if(one_buff_len*(_buf_cnt+1)>MEM_USE_LIMIT){
+                        /*buffer不可以无限制的加，当到一定阈值的时候，异常退出*/
+                        cur_buf=cur_buf->next;
+                        lst_lts=curr_sec;
+                    }
+                    else
+                    {
+                        /*第next个buffer日志线程在工作，因此添加一个buffer*/
+                        cell_buffer* new_buf = new cell_buffer(one_buff_len);
+                        new_buf->next=next_buf;
+                        cur_buf->next=new_buf;
+                        next_buf->pre=new_buf;
+                        new_buf->pre=cur_buf;
+                        cur_buf=cur_buf->next;
+                        _buf_cnt++;
+                    }
+                }
+                else{
+                    /*第next个buffer没有日志线程，数据写到此缓冲区上*/
+                    cur_buf=cur_buf->next;
+                } 
+                if(!lst_lts)
+                        cur_buf->append(log_line,len_all);
+            }
+            else{
+                /*当前buffer消费者正在把数据写到日志文件中，因为是循环链表，故此时所有的buffer均已用完，极少出现*/
+                lst_lts=curr_sec;
+            }
+        }
+    }
+    if(tell_back){
+        cond_.notify_one();
+    }
+}
 
 #define LOG_INIT(log_dir,basename,level) \
     do \
