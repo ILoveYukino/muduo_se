27ee0668ae80a7805da4a6c1d132c53e31d58f3b
805256689 <805256689@qq.com>
2020-12-09 23:14:15 +0800

    master

--- muduo_se/base/singleton.h
+++ muduo_se/base/singleton.h
@@ -1,4 +1,45 @@
 #ifndef _SINGLETON_H_
 #define _SINGLETON_H_
 
+/*
+    懒汉模式-单例模板类，实现Singleton<T>的单例
+    自定义输入参数
+    智能指针管理单例资源
+    要求再次调用时，单例类最好有个输入参数为空的构造函数，或者输入参数有默认参数，或者任意赋值，并不改变单例中成员变量
+*/
+
+#include <iostream>
+#include <string>
+#include <thread>
+#include <mutex>
+
+template <typename T>
+class Singleton{
+    public:
+        template <typename... Args>
+        static T& instance(Args&&... args){
+            std::call_once(flag_,[&](){
+                instance_= std::make_unique<T>(std::forward<Args>(args)...);
+            });
+            return *instance_;
+        }
+
+    private:
+        Singleton()=default;
+        ~Singleton()=default;
+        Singleton(const Singleton&)=delete;
+        Singleton& operator=(const Singleton&)=delete;
+
+        static std::unique_ptr<T> instance_;
+        static std::once_flag flag_;
+};
+
+template <typename T>
+std::unique_ptr<T> Singleton<T>::instance_;
+
+template <typename T>
+std::once_flag Singleton<T>::flag_;
+
+
+
 #endif
--- muduo_se/test/singleton.cc
+++ muduo_se/test/singleton.cc
@@ -1,5 +1,67 @@
 #include <iostream>
+#include <base/singleton.h>
+
+class A{
+    public:
+    A(int a,int b):a_(a),b_(b){
+
+    }
+
+    A(){
+
+    }
+
+    void s(){
+        std::cout<<" test B class "<<a_<<" "<<b_<<std::endl;
+    }
+
+    void setab(int a,int b){
+        a_=b;
+        b_=b;
+    }
+
+    int a_;
+    int b_;
+};
+
+class B{
+    public:
+    void s(){
+        std::cout<<" test B class "<<std::endl;
+    }
+};
+
+/*int main(){
+    A& a=Singleton<A>::instance(3,4);
+    B& b=Singleton<B>::instance();
+
+    a.s();
+    b.s();
+
+    return 0;
+}*/
+
+void func(){
+    std::cout<<"tid = "<<std::this_thread::get_id()<<" start"<<std::endl;
+
+    A& a=Singleton<A>::instance(3,3);
+
+    a.s();
+
+    a.setab(2,2);
+
+    a.s();
+
+    std::cout<<"tid = "<<std::this_thread::get_id()<<" end"<<std::endl;
+}
 
 int main(){
+    std::cout<<"tid = "<<std::this_thread::get_id()<<" start"<<std::endl;
     
+    Singleton<A>::instance(0,0);
+    std::thread t1(func);
+    t1.join();
+
+    std::cout<<"tid = "<<std::this_thread::get_id()<<" running"<<std::endl;
+    Singleton<A>::instance().s();
 }
